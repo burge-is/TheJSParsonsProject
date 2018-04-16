e77bd2d7bd6f73cd79d71aee3db013b243fb8c06
David Burgess <i.am@dburg.es>
2018-04-15 22:10:38 -0400

    Added logic for extra large puzzle piece holders

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 startApp();
-
+/*
+* Bootstrap the app: 
+* here we tie in the starter event handlers
+* and prepare the dom for drag/drop interactions
+*/
 function startApp() {
   const loadPenButton = document.getElementById("load-pen-button");
   loadPenButton.onclick = loadPenPieces;
-  prepareDragAreas();
+
+  prepareDragEvents();
+
+  // Update result iframe when the html/css/js areas change
   document.getElementById("editor-form").oninput = function() {
     updateFrame({
       iframe: document.getElementsByTagName("iframe")[0],
-      css: document.getElementById("css-editor").value,
-      html: document.getElementById("html-editor").value,
-      js: document.getElementById("js-editor").value
+      css: document.getElementById("css-editor").value || "",
+      html: document.getElementById("html-editor").value || "",
+      js: document.getElementById("js-editor").value || ""
     });
   };
+
+  // Handle toggling on/off the cheat iframe
   document.getElementById("piece-answer-swapper").onclick = function() {
-    document.getElementById("box-cover").style.display === "none"
+    document.getElementById("box-cover").style.display !== "block"
       ? (document.getElementById("box-cover").style.display = "block")
       : (document.getElementById("box-cover").style.display = "none");
   };
 }
+/*
+* Update an iframe HTMLElement with the provided css, html, and js
+*/
 function updateFrame({ iframe, css, html, js }) {
   const resultsFrameDoc = iframe.contentDocument;
   resultsFrameDoc.open();
@@ -39,10 +51,14 @@ function updateFrame({ iframe, css, html, js }) {
   );
   resultsFrameDoc.close();
 }
+/*
+* Load the pen pieces from the codepen-url input text
+* This code will grab the .html .css and .js files
+* from codepen and convert them into puzzle pieces
+*/
 function loadPenPieces() {
-  const button = document.getElementById("load-pen-button");
   const codepenURL =
-    button.parentElement.querySelector("#codepen-url").value ||
+    document.querySelector("#codepen-url").value ||
     "https://codepen.io/andcircus/pen/aYgxOy";
   const html = get(codepenURL + ".html");
   const css = get(codepenURL + ".css");
@@ -55,24 +71,39 @@ function loadPenPieces() {
 
   Promise.all([html, css, js]).then(function(responses) {
     document.getElementById("piece-holder").innerHTML = "";
-    updateFrame({
-      iframe: document.getElementById("box-cover"),
-      html: responses[0],
-      css: responses[1],
-      js: responses[2]
-    });
+    // updateFrame({
+    //   iframe: document.getElementById("box-cover"),
+    //   html: responses[0],
+    //   css: responses[1],
+    //   js: responses[2]
+    // });
     responses
+      // Flatten the js, html, and css into a single array
+      // of text based on \n in each of the returned files
       .reduce(function(arr, resp) {
         return arr.concat(
           resp.split("\n").filter(function(line) {
-            return line !== "";
+            return line.trim() !== "";
           })
         );
       }, [])
+      //build a draggable html element from each of these lines
       .map(buildDraggable)
+      //lastly add these lines to the puzzle piece holder
       .map(a => document.getElementById("piece-holder").appendChild(a));
+
+    const pieceHolderSize = document.getElementById("piece-holder")
+      .scrollHeight;
+    document.getElementById("box-cover").src =
+      codepenURL.replace("/pen/", "/embed/") +
+      "/?height=" +
+      (pieceHolderSize < 800 ? pieceHolderSize : 800) +
+      "&default-tab=result";
   });
 }
+/*
+*  Create a draggable HTMLElement out of text
+*/
 function buildDraggable(text) {
   let draggable = document.createElement("pre");
   draggable.draggable = true;
@@ -81,7 +112,14 @@ function buildDraggable(text) {
 
   return draggable;
 }
-function prepareDragAreas() {}
+/*
+* Handle puzzle drag events
+*/
+function prepareDragEvents() {}
+
+/*
+* Old school xhr get
+*/
 function get(url) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
